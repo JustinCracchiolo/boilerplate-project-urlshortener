48ae71d6d0895b35f9135b646e4bcc9ea2fa8fbf
justincracchiolo <justincracchiolo@gmail.com>
2026-09-04 20:54:13 -0400

    implement get and post api

--- index.js
+++ index.js
@@ -14,10 +14,53 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+let urlDatabase = {}
+let counter =  1
+
+// /api/shortul POST
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+
+  // Validate URL format
+  try {
+    const parsedUrl = new URL(originalUrl);
+
+    // Must start with http or https
+    if (parsedUrl.protocol !== "http:" && parsedUrl.protocol !== "https:") {
+      return res.json({ error: "invalid url" });
+    }
+
+    // DNS lookup to verify hostname
+    dns.lookup(parsedUrl.hostname, (err) => {
+      if (err) {
+        return res.json({ error: "invalid url" });
+      }
+
+      const shortUrl = counter++; //input into hasmap must not be mutable (hence the need to make a const variable)
+      urlDatabase[shortUrl]  = originalUrl
+
+      res.json({
+        originalUrl: originalUrl,
+        shortUrl: shortUrl
+      })
+    })
+  } catch (err) {
+    return res.json({error: "invalid url"})
+  }
 });
+
+// /api/shorturl/:shorturl GET 
+app.get("/api/shorturl/:shorturl", (req, res) => {
+  const shortUrl = req.params.shorturl
+  const originalUrl = urlDatabase[shortUrl]
+
+  if (!originalUrl) {
+    return res.json({ error: "invalid url" });
+  }
+
+  res.redirect(originalUrl)
+
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
